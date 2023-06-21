ce86efa8dd285f8594894abb98d3d26a6a946af4
Lucas Gladding <lucas@me.com>
2023-06-21 13:13:28 -0400

    Added first date test

---
+++ src/components/DateText.spec.ts
@@ -0,0 +1,16 @@
+import {describe, it} from 'vitest';
+import {mount} from '@vue/test-utils';
+import {DateTime} from 'luxon';
+import DateText from './DateText.vue';
+
+describe('DateText', () => {
+    it('renders a date', () => {
+        const date = DateTime.now();
+        const wrapper = mount(DateText, {
+            props: {
+                date,
+            },
+        })
+        expect(wrapper.exists()).toEqual(true);
+    });
+});
---
+++ src/components/DateText.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {DateTime} from 'luxon';
+
+defineProps<{
+  date: DateTime,
+}>();
+</script>
+
+<template>
+  <div>{{date.toFormat('y')}}</div>
+</template>
